93350967b5ebde082ed163f6fd607cfbad059e56
Yolk <chinloveyoualot@gmail.com>
2023-12-06 10:24:53 +0700

    fourth update

--- restaurant.cpp
+++ restaurant.cpp
@@ -18,52 +18,68 @@ void simulate(string filename)
 	return;
 }
 //SORT FREQUENCY
-int check(vector<pair<int,int>> ys,int x){
-    for(size_t i=0;i<ys.size();i++){
-        if(ys[i].first==x)
-            return i;
-    }
-    return -1;
-}
-void insertsort(vector<pair<int,int>> &ys){
-    for(size_t i=1;i<ys.size();i++){
-        int tmp=ys[i].second;
-        int tmp1=ys[i].first;
-        int j;
-        for(j=i;j>0 && ys[j-1].second<tmp;j--){
-            ys[j].second=ys[j-1].second;
-            ys[j].first=ys[j-1].first;
-        }
-        ys[j].second=tmp;
-        ys[j].first=tmp1;
-    }
-}
+// int check(vector<pair<int,int>> ys,int x){
+//     for(size_t i=0;i<ys.size();i++){
+//         if(ys[i].first==x)
+//             return i;
+//     }
+//     return -1;
+// }
+// void insertsort(vector<pair<int,int>> &ys){
+//     for(size_t i=1;i<ys.size();i++){
+//         int tmp=ys[i].second;
+//         int tmp1=ys[i].first;
+//         int j;
+//         for(j=i;j>0 && ys[j-1].second<tmp;j--){
+//             ys[j].second=ys[j-1].second;
+//             ys[j].first=ys[j-1].first;
+//         }
+//         ys[j].second=tmp;
+//         ys[j].first=tmp1;
+//     }
+// }
 
-void sortByFrequency(int arr[], int n){
-    vector<pair<int,int>> ys;
-    ys.emplace_back(arr[0],1);
-    
-    for(int i=1;i<n;i++){
-        int h=check(ys,arr[i]);
-        if(h==-1){
-           ys.emplace_back(arr[i],1);
-        }
-        else {
-            ys[h].second++;
-        }
-    }
-    insertsort(ys);
-    int j=0;
-    for(size_t i=0;i<ys.size();i++){
-        while(ys[i].second>0){
-            arr[j++]=ys[i].first;
-            ys[i].second--;
-        }
-    }
-}
+// void sortByFrequency(int arr[], int n){
+//     vector<pair<int,int>> a;
+//     a.emplace_back(arr[0],1);
+//     for(int i=1;i<n;i++){
+//         int h=check(a,arr[i]);
+//         if(h==-1){
+//            a.emplace_back(arr[i],1);
+//         }
+//         else {
+//             a[h].second++;
+//         }
+//     }
+//     insertsort(a);
+//     int j=0;
+//     for(size_t i=0;i<a.size();i++){
+//         while(a[i].second>0){
+//             arr[j++]=a[i].first;
+//             a[i].second--;
+//         }
+//     }
+// }
 //END SORT FREQUENCY
 void LAPSE(string name){
-
+    unordered_map<char, int>mp;
+    for (auto i : name) {
+        mp[i]++;
+    }
+    priority_queue<pair<int, char>>pt;
+    for (auto i : mp) {
+        pt.push({ i.second,i.first });
+    }
+    string ans = "";
+    while (!empty(pt)) {
+        int val = pt.top().first;
+        while (val) {
+            ans += pt.top().second;
+            val--;
+        }
+        pt.pop();
+    }
+    
 }
 
 void KOKUSEN(){
